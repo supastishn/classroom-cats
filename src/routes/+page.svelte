fc1c8a23a50e0b811e0a997c70e7dfd9d501f687
supastishn <omareeto2012@hotmail.com>
2025-02-07 22:10:52 +0300

    add baskc functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+import {onMount} from 'svelte'
+onMount(async () => {
+
+const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+const audioContext = new AudioContext();
+const mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(stream);
+const analyserNode = audioContext.createAnalyser();
+mediaStreamAudioSourceNode.connect(analyserNode);
+
+const pcmData = new Float32Array(analyserNode.fftSize);
+const onFrame = () => {
+    analyserNode.getFloatTimeDomainData(pcmData);
+    let sumSquares = 0.0;
+    for (const amplitude of pcmData) { sumSquares += amplitude*amplitude; }
+    const volume = Math.sqrt(sumSquares / pcmData.length);
+    console.log(volume)
+    if (volume >= 0.1) {
+	document.getElementById("awake").style.display = "block"
+	document.getElementById("asleep").style.display = "none"
+	console.log("oops the cat is awake")
+	setTimeout(() => {
+		document.getElementById("awake").style.display = "none"
+		document.getElementById("asleep").style.display = "block"
+	}, 5000)
+    }
+    window.requestAnimationFrame(onFrame);
+};
+setInterval(onFrame, 100)
+
+})
+
+</script>
+
+
+Don't talk in order to keep the cats asleep!
+<img src="https://images.saymedia-content.com/.image/t_share/MTc0Mjg1ODg4NjEwNjQxNzg4/small-exotic-cats.jpg" id="awake"/>
+<img src="https://images.rawpixel.com/image_png_social_landscape/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAyL3BkbWlzYzgtZm5nMzA1NjU3Yi1pbWFnZS5wbmc.png" id="asleep"/>
+
+<style>
+#awake {
+	display: none;
+}
+</style>
