83d5385355e3034b433a63e2c9d939a15f997e74
supastishn <omareeto2012@hotmail.com>
2025-02-08 22:44:35 +0300

    Add gif

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,7 +52,7 @@ Volume threshold (default is 25)
 <input type="range" min="1" max="100" value="25" class="slider" id="myRange">
 <p id="val">25</p>
 <img src="https://images.saymedia-content.com/.image/t_share/MTc0Mjg1ODg4NjEwNjQxNzg4/small-exotic-cats.jpg" id="awake"/>
-<img src="https://images.rawpixel.com/image_png_social_landscape/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAyL3BkbWlzYzgtZm5nMzA1NjU3Yi1pbWFnZS5wbmc.png" id="asleep"/>
+<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/78d89d89-9029-4e4c-a587-6cd2c270631c/d7hcigh-1c33f2a8-b8e8-431b-841a-9ac9543913bd.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi83OGQ4OWQ4OS05MDI5LTRlNGMtYTU4Ny02Y2QyYzI3MDYzMWMvZDdoY2lnaC0xYzMzZjJhOC1iOGU4LTQzMWItODQxYS05YWM5NTQzOTEzYmQuZ2lmIn1dXX0.aGufalnylEW1hqKHk7H1n2kikKdiqcuZEb8h8wegXK8" id="asleep"/>
 
 <style>
 #awake {
