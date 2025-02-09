525b3329ef380d7a9530103bd80abd8673109cdc
supastishn <omareeto2012@hotmail.com>
2025-02-09 16:44:40 +0300

    .

---
+++ _app/immutable/chunks/D0GQsAjN.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_ft5vdr)==null?void 0:s.base)??"/classroom-cats";var t;const e=((t=globalThis.__sveltekit_ft5vdr)==null?void 0:t.assets)??a;export{e as a,a as b};
---
+++ _app/immutable/entry/start.CjcWhmRE.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/aYrT1s3q.js";export{t as start};
