f43b63529737c89a5903613798b4ef811f134a99
supastishn <omareeto2012@hotmail.com>
2025-02-09 16:43:56 +0300

    add runnin away

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 import {onMount} from 'svelte'
-
+import { base } from '$app/paths';
 export const ssr = false
 onMount(async () => {
 
@@ -24,10 +24,30 @@ const onFrame = () => {
     console.log(volume)
     console.log("Threshold: " + document.getElementById("myRange").value * 0.04)
     if (volume >= document.getElementById("myRange").value * 0.004) {
+    clearTimeout(window.catSleep)
+
+	if (volume >= document.getElementById("myRange").value * 0.008) {
+	clearTimeout(window.catCome)
+	document.getElementById("run").style.display = "block"
+	document.getElementById("awake").style.display = "none"
+        document.getElementById("asleep").style.display = "none"
+        console.log("oops the cat is RUNNING WHAT HAVE YOU DONE")
+       window.catCome = setTimeout(() => {
+		document.getElementById("run").style.display = "none"
+                document.getElementById("awake").style.display = "none"
+                document.getElementById("asleep").style.display = "block"
+                //make meow tbd
+
+
+
+        }, 3000)
+
+	return
+	}
 	document.getElementById("awake").style.display = "block"
 	document.getElementById("asleep").style.display = "none"
 	console.log("oops the cat is awake")
-	setTimeout(() => {
+	window.catSleep = setTimeout(() => {
 		document.getElementById("awake").style.display = "none"
 		document.getElementById("asleep").style.display = "block"
 		//make meow tbd
@@ -36,7 +56,7 @@ const onFrame = () => {
 		
 	}, 5000)
     }
-    window.requestAnimationFrame(onFrame);
+   // window.requestAnimationFrame(onFrame);
 };
 setInterval(onFrame, 100)
 
@@ -53,9 +73,12 @@ Volume threshold (default is 25)
 <p id="val">25</p>
 <img src="https://images.saymedia-content.com/.image/t_share/MTc0Mjg1ODg4NjEwNjQxNzg4/small-exotic-cats.jpg" id="awake"/>
 <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/78d89d89-9029-4e4c-a587-6cd2c270631c/d7hcigh-1c33f2a8-b8e8-431b-841a-9ac9543913bd.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi83OGQ4OWQ4OS05MDI5LTRlNGMtYTU4Ny02Y2QyYzI3MDYzMWMvZDdoY2lnaC0xYzMzZjJhOC1iOGU4LTQzMWItODQxYS05YWM5NTQzOTEzYmQuZ2lmIn1dXX0.aGufalnylEW1hqKHk7H1n2kikKdiqcuZEb8h8wegXK8" id="asleep"/>
-
+<img src="{base}/cat.gif" id="run"/>
 <style>
 #awake {
 	display: none;
+}
+#run {
+        display: none;
 }
 </style>
